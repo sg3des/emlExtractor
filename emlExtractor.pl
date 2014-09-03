6a30fa3e31ed029cae2766d6e380ead6561ad853
sg3des <sg3des@gmail.com>
2014-09-03 14:03:51 +0400

    refactoring

--- emlExtractor.pl
+++ emlExtractor.pl
@@ -26,6 +26,7 @@ my $charset;
 
 print "\n";
 foreach $filePath(@ARGV){
+	# if(length($filePath)<=1){return;}
 	openEml($filePath);
 }
 
@@ -35,7 +36,6 @@ sub openEml
 	my($path) = @_;
 	$folder = $path;
 	print color 'bold green';	print $path."\n";	print color 'reset';
-	
 	my $fh = new IO::File "< $path" or die "Cannot open $path : $!";
 	flock($fh,LOCK_SH);
 	binmode($fh);
@@ -43,74 +43,147 @@ sub openEml
 	my $buflen = (stat($fh))[7];
 
 	while (read($fh,$buf,$buflen)) {
-		$delimter = searchDelimter($buf);
-		
-		my $boundary = getBoundary($buf);
-		explode($buf,$boundary);
+		$boundary = getBoundary($buf);
 
-		# header($buf);
+		if(!$boundary){$boundary = 'Content-Type:';}
+		explode($buf,$boundary);
 	}
 	print "\n";
 }
 
-
 sub explode
 {
 	my($buf, $boundary) = @_;
+	$step=0;
 	
 	foreach $part (split $boundary, $buf){
-		my $filename='',$type='',$textType='';
-		foreach $line (split $delimter, $part){
-			if($line =~ /filename/)			{$filename = $line;}
-			if($line =~ /binary/)				{$type = 'binary';}
-			if($line =~ /base64/)				{$type = 'base64';}
-			if($line =~ /msword/)				{$ext = 'doc';}
-			if($line =~ /text\/plain/)	{$textType = 'text';}
-			if($line =~ /text\/html/)		{$textType = 'html';}
+		$step++;
+		# print $part;
+		if($step==1){header($part);next;}
+		if($part =~ m/(^Content\-Type.*text\/plain;)/mgi){mailText($part,'text'); next;}
+		if($part =~ m/(^Content\-Type.*text\/html;)/mgi){mailText($part,'html'); next;}
+		if($part =~ m/(^Content\-Disposition.*attachment;)/mgi){attachment($part); next;}
 
-			if($line =~ /koi8-r/i)			{$charset = 'koi8-r';}
-			if($line =~ /utf-8/i)				{$charset = 'utf-8';}
-			if($line =~ /windows-1251/i){$charset = 'windows-1251';}
-
-			# if($charset){$TextIconv = Text::Iconv->new($charset, 'utf-8')}
-			if($line =~ /boundary/i)		{if(!$cropHeader){$cropHeader=$part;}}
-		}
-		if (length($filename)>1 && length($type)>1){
-			attachment($part,$filename,$type,$ext);
-		}
-		if(length($textType)>1){
-			print "body.".$textType."\n";
-			mailText($part,$textType);
-		}
+		# if($part =~ m/^Content-Disposition:attachment;/mgi){attachment($part); next;}
+		# if($part =~ m/^Content-Type:text\/plain;/mgi){mailText($part,'text'); next;}
+		# if($part =~ m/^Content-Type:text\/html;/mgi){mailText($part,'html'); next;}
 	}
-	if($cropHeader){header($cropHeader);}
 }
 
 sub header
 {
-	my($partHeader) = @_;
-	my %header;
-	foreach my $line (split m/\r|\n/, $partHeader){
-		if($line =~ m/^From:/i && $line =~ m/@/){$header{'from'}=$line;}
-		if($line =~ m/^To:/i  && $line =~ m/@/)	{$header{'to'}=$line;}
-		if($line =~ m/^Subject:/i)							{$header{'subject'}=$line;}
-		if($line =~ m/^Date:/i)									{$header{'date'}=$line;}
+	my($part) = @_;
+	$header .= (($part =~ m/(^from:.*$)/mgi)[0])."\n";
+	$header .= (($part =~ m/(^to:.*$)/mgi)[0])."\n";
+	$header .= (($part =~ m/(^subject:.*$)/mgi)[0])."\n";
+	$header .= (($part =~ m/(^cc:.*$)/mgi)[0])."\n";
+	saveFile('eml.header',$header);
+	return;
+}
+sub mailText
+{
+	my($part,$type) = @_;
+	# @content = content($part);
+	@part = cropContent($part);
+	@content = content(@part[0]);
+
+
+	$part = absoluteDecode(@part[1],@content);
+	if($type eq 'html'){$part =~ s/@content[0]/utf-8/gi;}
+	$filename = $type.".".$type;
+	# print $filename;
+	saveFile($filename,$part);
+}
+
+sub attachment
+{
+	my($part) = @_;
+	@part = cropContent($part);
+	@content = content(@part[0]);
+	$filename = (@part[0] =~ m/filename="(.*)"/gi)[0];
+	$filename = string_decode($filename);
+
+	if($filename !~ m/\./){
+		$ext = (@part[0] =~ m/^Content-Type:.*\/(.*);/mgi)[0];
+		$filename.=".".$ext;
 	}
 
-	foreach $k (keys %header){
-		@replace = ('From: ','To: ','Subject: ','Date: ');
-		foreach $replace (@replace){
-			$header{$k} = str_replace($replace,'',$header{$k});
-		}
-		$header{$k} = string_decode($header{$k});
-		%result = (%result,$k.": ".$header{$k}."\n");
-	}
-	$header = join("",%result);
-	if($header){
-		saveFile('eml.header',$header);
-		print "eml.header\n";
-	}
+	if(@content){$part = absoluteDecode(@part[1],@content);}
+	saveFile($filename,$part);
+	
 	return;
+}
+sub content
+{
+	my($part) = @_;
+	$charset = ($part =~ m/charset="(.*)"/mgi)[0];
+	$encoding= ($part =~ m/^Content-Transfer-Encoding:(.*)$/mgi)[0];
+	return ($charset,$encoding);
+}
+sub absoluteDecode
+{
+	my($part,@content) = @_;
+	$charset = @content[0];
+	$encoding = @content[1];
+
+	if($encoding=~ m/base64/i){$part=decode_base64($part);}
+	if($encoding=~ m/quoted-printable/i){$part=decode_qp($part);}
+	if($charset){$part=decode($charset,$part);}
+
+	return $part;
+}
+
+sub getBoundary
+{
+	my($buf) = @_;
+	$boundary = ($buf =~ m/\t+boundary="(.*)"/i)[0];
+	if(!$boundary){return;}
+	$boundary = "--".substr($boundary,0,10); #берем только первые 10 симоволов, это нужно потому что бывает несколько баундарев и обычно различаются они где-то не в начале.
+	return $boundary;
+}
+
+
+sub cropContent
+{
+	my($part) = @_;
+
+	# print $part;
+	# $crop = index($part,"eJ8");
+	# if($crop+1){substr($part,0,$crop)='';}
+
+	if(index($part,"\r")+1){$delimter="\r"; $plus=1;}else{$delimter="\n";$plus=0;}
+
+
+	my $num=0;
+	my $cropLength=0;
+	my $startSchDelimter=0;
+	_CROP:	
+	foreach my $line (split $delimter, $part){
+		$num++;
+		$length = length($line)+1;
+		$cropLength += $length;
+		if($line =~ /charset/i){$startSchDelimter = 1;}
+		if($line =~ /encoding/i){$startSchDelimter = 1;}
+		if($startSchDelimter>0 && $length <= 2){
+			$cropLine=$num; 
+			last _CROP;
+		}
+	}
+	# print $cropLength."\n";
+	$content = substr($part,0,$cropLength+$plus);  #плюс нужен изза разновидности переноса строк 
+	substr($part,0,$cropLength+$plus)='';
+
+
+	return ($content,$part);
+
+
+	# $crop = index($part,$delimter,$startCrop);
+
+	# print $startCrop." ".$crop."\n";
+	# $part =~ s/(^Content.*$)|(^\t.*$)//mgi; # убираем все строки которые начинаются со слова Content или с таба скорее всего это заголовок
+	# $part =~ s/\r$\r$|^\n^\n//mgi; #убираем двойные переносы строк
+
+	# return $part;
 }
 
 sub string_decode
@@ -118,173 +191,55 @@ sub string_decode
 	my ($string) = @_;
 	my @result = ();
 	$email = '';
-
 	if($string =~ m/@/){
 		@email = ($string =~ m/(\<+[\w]+\@+[\w]+\.+[\w]+\>)/);
 		$email = join '',@email;
 		$string =~ s/$email//;
 	}
-
+	if($string =~ /windows-1251/) {$charset = 'windows-1251';}
+	if($string =~ /koi8-r/) {$charset = 'koi8-r';}
 	if($string =~ m/$charset/){
 		if($string =~ m/\?B\?/){$quoted = 'base64';}
 		if($string =~ m/\?Q\?/){$quoted = 'qp';}
-		# print "   ".$string."\n";
-			$string =~ s/[^\w\.\s\=\?\+\-\/]//gi; #убираем непечатные символы
-		
-
+		$string =~ s/[^\w\.\s\=\?\+\-\/]//gi; #убираем непечатные символы
 		$string =~ s/\=\?+$charset|\?B\?|\?\=//gi;
-		
-
 		if($quoted eq 'base64'){$string=decode_base64($string);}
 		if($quoted eq 'qp'){$string=decode_qp($string);}
-		$string = decode($charset,$string);
-		# $string = from_to($string, $charset, 'utf-8');
-		# $string = $TextIconv->convert($string);
-		# print " > ".$string." ".$charset." ".$quoted."\n";
+		if($charset){$string = decode($charset,$string);}
 	}
-	$string = $string." ".$email;
+	if($email){$string = $string." ".$email;}
 	return $string;
-}
-
-sub mailText{
-	my($part, $type) = @_;
-
-	$part = cropGarbage($part,$delimter);
-
-	@part = split($delimter,$part);pop @part;$part = join("",@part); #delete last line!
-
-	$part = decode_qp($part);
-	$part = decode($charset,$part);
-
-	# $part = from_to($part, $charset, 'utf-8');
-	# $part = $TextIconv->convert($part);
-
-	# $testString = '1tPRINHu6ujw/+3o';
-	# $testString = decode_base64($testString);
-	# print decode($charset,$testString)."\n";
-	# print $test = ($TextIncov->convert($testString))."\n";
-	# print Convert::Cyrillic::cstocs('windows-1251', 'UTF8', $testString)."\n";
-
-	if($type eq 'html'){$part = str_replace($charset,'utf-8',$part);}
-
-	$filename = $type.".".$type;
-	saveFile($filename,$part);
-}
-
-sub attachment
-{
-	my($document, $filename, $type, $ext) = @_;
-	# print "   ".$filename." ".$ext."\n";
-	if($filename =~ /koi8-r/) {$charset = 'koi8-r'; $filename = str_replace('=?koi8-r','',$filename);}
-	if($filename =~ /windows-1251/) {$charset = 'windows-1251'; $filename = str_replace('=?windows-1251','',$filename);}
-
-	if($filename =~ /\?B\?/) {$quoted = 'base64'; $filename = str_replace('?B?','',$filename);}
-	if($filename =~ /\?Q\?/) {$quoted = 'qp'; $filename = str_replace('?Q?','',$filename);}
-	
-
-
-	$filename = str_replace('?=','',$filename);
-	$filename = str_replace('filename=','',$filename);
-
-	$filename = str_replace('"','',$filename);
-	$filename	=~ s/^\s+|\s+$//g;
-	$filename = str_replace(' ','_',$filename);
-	
-	$filename	=~ s/\r|\n//g;
-	# print " > ".$filename."\n";
-
-	# $filename =~ s/[\)\(\"\'\?]|filename|//gi;
-	if($quoted eq 'base64'){$filename=decode_base64($filename);}
-	if($quoted eq 'qp'){$filename=decode_qp($filename);}
-	$filename = decode($charset,$filename);
-
-	$filename =~ s/[^\w\.\r ]//gi; #убираем непечатные символы
-	if($filename !~ m/\./){$filename = $filename.".".$ext;}
-	print $filename."\n";
-	$document = cropGarbage($document,$delimter);
-	if($type eq 'base64'){
-		$document = MIME::Base64::decode($document);
-	}
-
-	saveFile($filename,$document);
-
-	# if($filename =~ /.dat/){
-	# 	$exec = 'tnef --overwrite '.$filename;
-	# 	exec $exec;
-	# }
-	return;
-}
-
-sub searchDelimter
-{
-	my($document) = @_;
-	@delimters = ("\r","\n");  # todo: add type of delimters;
-	foreach $delimter(@delimters){
-		$index = index($document,$delimter);
-		if($index+1) {return $delimter;}
-	}
-	
-}
-
-sub search
-{
-	my ($text,$what,$index) = @_;
-	return index($text,$what,$index);
-}
-
-
-sub getBoundary
-{
-	my($buf) = @_;
-	_BOUNDARY:
-	foreach my $line (split m/\n+/, $buf){
-		if($line =~ /boundary/) {
-			$boundary=$line; 
-			last _BOUNDARY;
-		}
-	}
-	if(!$boundary){print 'boundary not found'; return;}
-	$boundary = str_replace('boundary="','',$boundary);
-	chop $boundary;
-	@boundary = split(/-|_|=|"|\s/, $boundary);
-	_BOUNDARY:
-	foreach my $partBoundary (@boundary){
-		if($partBoundary) {
-			return $partBoundary;
-			last _BOUNDARY;
-		}
-	}
 }
 
 sub cropGarbage
 {
-	my($part, $delimter) = @_;
+	my($part,$delimter) = @_;
+
 	my $num=0;
 	my $cropLength=0;
 	my $startSchDelimter=0;
-	
-	_CROP:
-	foreach my $line (split $delimter,$part){
+	_CROP:	
+	foreach my $line (split $delimter, $part){
 		$num++;
 		$length = length($line)+1;
 		$cropLength += $length;
-		if($line =~ /Content-Type/){$startSchDelimter = 1;}
+		if($line =~ /charset/){$startSchDelimter = 1;}
 		if($startSchDelimter>0 && $length <= 2){
 			$cropLine=$num; 
 			last _CROP;
 		}
 	}
+	
 	if($delimter eq "\r"){$plus = 1;}else{$plus=0;} #плюс нужен изза разновидности переноса строк 
 	substr($part,0,$cropLength+$plus)='';
+
 	return $part;
 }
 
-my $fileNum=0;
 sub saveFile{
 	my ($filename,$document) = @_;
-	
 
-	$folder = str_replace('.eml','_eml',$folder);
+	$folder =~ s/\.eml/_eml/gi;
 	mkdir $folder;
 
 	my $sfh = new IO::File ">"."$folder/$filename" or die "Cannot open $filename : $!";
@@ -292,26 +247,7 @@ sub saveFile{
 	binmode($sfh);
 	print $sfh $document or die "Write to $filename failed: $!";
 	close($sfh) or die "Error closing $filename : $!";
+
+	print $filename."\n";
 	return '$filename OK';
-}
-
-
-
-
-sub str_replace
-{
-	my $replace_this = shift;
-	my $with_this  = shift; 
-	my $string   = shift;
-	
-	my $length = length($string);
-	my $target = length($replace_this);
-	
-	for(my $i=0; $i<$length - $target + 1; $i++) {
-		if(substr($string,$i,$target) eq $replace_this) {
-			$string = substr($string,0,$i) . $with_this . substr($string,$i+$target);
-			# return $string; #Comment this if you what a global replace
-		}
-	}
-	return $string;
 }
